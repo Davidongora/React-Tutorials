e40160d3b2e6ce5a9959cb857c6ccd217ab3e1e8
David <ongoradavid5@gmail.com>
2023-08-23 23:24:53 +0300

    wrapping elemnts in react fragments

---
+++ react2/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+function Hero () {
+    return (
+        <>
+            <h1>Welcome to our suparmarket</h1>
+            <p>online store</p>
+        </>
+    )
+}
+// const Hero =() => {
+
+// }
