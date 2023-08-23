752104a7a80eeee03c5f1f9c270a997bf390b4a6
David <ongoradavid5@gmail.com>
2023-08-23 23:14:00 +0300

    rendering a page using the React.createElement

---
+++ react2/root.js
@@ -0,0 +1,5 @@
+import {createRoot} from "react-dom/client";
+const root = document.querySelector("#root");
+createRoot(root).render(React.createElement("p", {}, "Hello World"));
+// console.log(react.cost);
+createRoot(root).render(elememt);
